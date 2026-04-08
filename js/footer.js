99a3bfaa2f366a9668757625170ffb205d91b742
feynman82 <feynman82@gmail.com>
2026-04-08 08:09:39 +0800

    Update footer.js

--- js/footer.js
+++ js/footer.js
@@ -21,6 +21,9 @@ class GlobalFooter extends HTMLElement {
                 <svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polyline points="6 9 12 15 18 9"></polyline></svg>
               </button>
               <div class="useful-links-menu" role="menu">
+                <a href="/pages/subject-mathematics.html">Mathematics →</a>
+                <a href="/pages/subject-science.html">Science →</a>
+                <a href="/pages/subject-english.html">English →</a>
                 <a href="/pages/subjects.html" role="menuitem">Subjects</a>
                 <a href="/pages/exam.html" role="menuitem">Exams</a>
                 <a href="/pages/tutor.html" role="menuitem">Miss Wena Tutor</a>
