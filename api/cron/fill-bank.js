3236993b99e376c07b014f604f0e6f5f4173d17a
feynman82 <feynman82@gmail.com>
2026-04-11 00:32:10 +0800

    Update fill-bank.js

--- api/cron/fill-bank.js
+++ api/cron/fill-bank.js
@@ -106,7 +106,7 @@ export default async function handler(req, res) {
         totalCloned += payload.length;
       }
     }
-
+ 
     return res.status(200).json({ success: true, cloned: totalCloned });
 
   } catch (err) {
